ac06134366b8fd3f5696825403fa8442981c26f6
Oreoluwa Alaba <alabaoreoluwa@gmail.com>
2025-04-30 06:21:24 +0100

    Fixed client-side routing on Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,26 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
 const App = () => {
+  const token = localStorage.getItem("access_token");
+
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/signup" />} />
+      <Route
+        path="/"
+        element={<Navigate to={token ? "/dashboard" : "/signup"} />}
+      />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/dashboard"
+        element={token ? <Dashboard /> : <Navigate to="/login" replace />}
+      />
+      <Route
+        path="*"
+        element={
+          <h1 className="text-center mt-20 text-2xl">404 - Page Not Found</h1>
+        }
+      />
     </Routes>
   );
 };
